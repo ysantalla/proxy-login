22fe40e04e45219d7d6f9df4e5028f9e2fe04d4c
Yasmany Santalla Pereda <ysantalla88@gmail.com>
2019-11-28 12:06:06 -0500

    Production commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlProxy: 'https://10.2.4.70:443/api/cmd',
+  urlProxy: 'https://proxy.upr.edu.cu:443/api/cmd',
   envName: 'Production',
   appName: 'PROXY-GO'
 };
